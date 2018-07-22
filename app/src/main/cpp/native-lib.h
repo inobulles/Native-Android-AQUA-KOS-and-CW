376ff01c2ee2bdc04ae88ef08032f8473cfed869
Obiwac <obiwac@gmail.com>
2018-07-22 09:56:43 +0200

    Added ALOGI macro in native-lib.h

--- app/src/main/cpp/native-lib.h
+++ app/src/main/cpp/native-lib.h
@@ -31,6 +31,7 @@
 #define LOG_TAG "AQUA"
 
 #define ALOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define ALOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
 #if VERBOSE_OUTPUT
 #define ALOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, __VA_ARGS__)
