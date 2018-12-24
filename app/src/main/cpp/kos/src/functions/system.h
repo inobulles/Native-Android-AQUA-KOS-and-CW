593d762f64df1931c500e01b6877e96da8551e7d
Obiwac <obiwac@gmail.com>
2018-12-24 11:14:56 +0100

    Now get_platform detects Android

--- app/src/main/cpp/kos/src/functions/system.h
+++ app/src/main/cpp/kos/src/functions/system.h
@@ -5,31 +5,35 @@
 	#include "../macros_and_inclusions.h"
 	
 	unsigned long long get_platform(void) {
-		#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-			return PLATFORM_WIN32;
-		#elif defined(_WIN64)
-			return PLATFORM_WIN64;
-		#elif defined(__CYGWIN__) && !defined(_WIN32)
-			return PLATFORM_UNIX;
-		#elif defined(__APPLE__) && defined(__MACH__)
-			printf("WARNING Apple based platform detected, consider changing your OS\n");
-			
-			#if TARGET_IPHONE_SIMULATOR == 1 || TARGET_OS_IPHONE == 1
-				return PLATFORM_IOS;
-			#elif TARGET_OS_MAC == 1
-				return PLATFORM_MACOS;
-			#else
-				return PLATFORM_CANCER;
-			#endif
-		#elif defined(BSD)
-			printf("WARNING BSD based system detected. Returning Unix, as BSD is not included in the AQUA system standard\n");
-			return PLATFORM_UNIX;
-		#elif defined(__linux__)
-			return PLATFORM_LINUX;
-		#elif defined(__unix__)
-			return PLATFORM_UNIX;
+		#if KOS_USES_JNI
+			return PLATFORM_ANDROID;
 		#else
-			return PLATFORM_OTHER;
+			#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+				return PLATFORM_WIN32;
+			#elif defined(_WIN64)
+				return PLATFORM_WIN64;
+			#elif defined(__CYGWIN__) && !defined(_WIN32)
+				return PLATFORM_UNIX;
+			#elif defined(__APPLE__) && defined(__MACH__)
+				printf("WARNING Apple based platform detected, consider changing your OS\n");
+
+				#if TARGET_IPHONE_SIMULATOR == 1 || TARGET_OS_IPHONE == 1
+					return PLATFORM_IOS;
+				#elif TARGET_OS_MAC == 1
+					return PLATFORM_MACOS;
+				#else
+					return PLATFORM_CANCER;
+				#endif
+			#elif defined(BSD)
+				printf("WARNING BSD based system detected. Returning Unix, as BSD is not included in the AQUA system standard\n");
+				return PLATFORM_UNIX;
+			#elif defined(__linux__)
+				return PLATFORM_LINUX;
+			#elif defined(__unix__)
+				return PLATFORM_UNIX;
+			#else
+				return PLATFORM_OTHER;
+			#endif
 		#endif
 		
 	}
